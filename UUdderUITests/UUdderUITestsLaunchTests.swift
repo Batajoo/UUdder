868c8a1c41f0cb7a768ff8a25bd529d24cae45df
Sanjiv <sanjibbataju@gmail.com>
2024-09-21 14:43:44 -0400

    Initial Commit

---
+++ UUdderUITests/UUdderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UUdderUITestsLaunchTests.swift
+//  UUdderUITests
+//
+//  Created by Sanjiv Batajoo on 9/21/24.
+//
+
+import XCTest
+
+final class UUdderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
